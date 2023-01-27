5b2c5e920005ae6b51bab63d34f691e61a3ad3fe
yakachang <yichen@nlplab.cc>
2023-01-27 21:49:01 +0800

    refactor: add prefix to test data and output

--- prep/mix_set_data.py
+++ prep/mix_set_data.py
@@ -16,7 +16,7 @@ dim_mapper = {
 def main():
 
     for set_id in [1, 2]:
-        for datatype in ["train", "dev"]:
+        for datatype in ["train", "dev", "test"]:
             df_list = []
             for dim in dim_mapper[set_id]:
                 df = pd.read_json(
@@ -24,14 +24,24 @@ def main():
                     lines=True,
                 )
                 df["text"] = df["text"].apply(lambda x: f"{dim}: {x}")
-                df_list.append(df)
+                if datatype == "test":
+                    df.to_json(
+                        f"../data/ASAP++/Original/set{set_id}/mix-dim/test_{dim}.json",
+                        orient="records",
+                        lines=True,
+                    )
+                else:
+                    df_list.append(df)
 
-            df_all = pd.concat(df_list)
-            df_all.sample(frac=1, random_state=42).to_json(
-                f"../data/ASAP++/Original/set{set_id}/mix-dim/{datatype}.json",
-                orient="records",
-                lines=True,
-            )
+            if datatype != "test":
+
+                df_all = pd.concat(df_list)
+
+                df_all.sample(frac=1, random_state=42).to_json(
+                    f"../data/ASAP++/Original/set{set_id}/mix-dim/{datatype}.json",
+                    orient="records",
+                    lines=True,
+                )
 
 
 if __name__ == "__main__":
