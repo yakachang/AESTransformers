468b402778a4c480af82f0d76a8af28172e2a7bf
yakachang <yichen@nlplab.cc>
2023-02-02 21:35:24 +0800

    feat: extract features for essays in ASAP dataset

--- features/extract_features.py
+++ features/extract_features.py
@@ -1,6 +1,31 @@
+import pandas as pd
 import dimensional_analyzer
 
-text = "Today is a good day. I am look forward to see you!"
 
-feedback_info = dimensional_analyzer.analyze_text(text)
-print(feedback_info)
+def collect_dataset(input_file):
+
+    df_ori = pd.read_csv(input_file, sep="\t", encoding="ISO-8859-1")
+    df = df_ori[["essay_id", "essay_set", "essay", "domain1_score"]]
+    df.columns = ["essay_id", "prompt_id", "text", "label"]
+
+    return df
+
+
+def main():
+
+    input_file = "../../data/ASAP/kaggle-asap-aes/training_set_rel3.tsv"
+    dataset = collect_dataset(input_file)
+
+    dataset["feedback"] = dataset["text"].apply(
+        lambda text: dimensional_analyzer.analyze_text(text)
+    )
+
+    dataset.to_json(
+        "../data/ASAP/features/ASAP+features.jsonl",
+        orient="records",
+        lines=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
